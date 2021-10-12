f091eedc1857bce0a5cb97fc9046bd8139301776
Weam Hayadry <weam.hiadry3@gmail.com>
2021-10-12 01:11:20 -0700

    main

---
+++ javascript.js
@@ -0,0 +1,30 @@
+//Java Script calculator
+
+// var number1 = 0;
+// var number2 = 0;
+// var operation =0;
+// var result =0 ;
+
+// number1 = window.prompt("enter a number: ");
+
+// operation = window.prompt("To Sum: 1 , To subtract: 2 ,to multiple: 3 , to divide: 4");
+
+// number2 = window.prompt("Enter another number: ");
+// number2= parseInt(number2);
+// number1= parseInt(number1);
+// result= parseInt(result);
+// if (operation == 1){
+//  result= number1+ number2;
+// }
+// else if (operation == 2){
+//     result=number1-number2;
+// }
+// else if (operation == 3){
+//     result=number1*number;
+// }
+// else if (operation == 4){
+//     result=number1/number2;
+// }
+// alert("The result of calclating is : "+result);
+
+
